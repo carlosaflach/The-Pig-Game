186212a20c1b2c5390934b8384ad3266153a27a1
Carlos Afonso <carlos.flach15@gmail.com>
2021-12-27 20:57:12 -0300

    let, const and var.

--- Fundamentals-part1/script.js
+++ Fundamentals-part1/script.js
@@ -31,19 +31,34 @@
 
     console.log("Hi my name is: " + firstName + " eu sou do: "+ country + ", localizado no continente: "+ continent + ", com uma população de: " + population+" de habitantes");
 
-    */
-  let javascriptIsFun = true;
+    let javascriptIsFun = true;
     // console.log(javascriptIsFun);
     console.log(typeof javascriptIsFun);
     // console.log(typeof 'Carlos');
     // console.log(typeof 23);
-
+    
     javascriptIsFun = "yes!"
     console.log(typeof javascriptIsFun);
-
+    
     let year;
-
+    
     console.log(year);
     console.log(typeof year);
-
+    
     console.log(typeof null); // error from javascript;
+
+    
+
+    let age = 30; // 
+    age = 31;
+
+    const birthYear = 1993; // We can't redeclarete a const variable.
+    // birthYear = 1991;
+
+    // const job; 
+    // we can't declarete a variable const empty.
+
+    lastName = "Flach"
+
+    console.log(lastName);
+*/
