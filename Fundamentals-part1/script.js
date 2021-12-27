76b1e18460b09f8bec1ea0b92c34f8b3e950d6a3
Carlos Afonso <carlos.flach15@gmail.com>
2021-12-27 20:31:08 -0300

    Values and Variables.

--- Fundamentals-part1/script.js
+++ Fundamentals-part1/script.js
@@ -1,5 +1,32 @@
 let js = 'amazing';
-    if (js === 'amazing') alert("JavaScript is FUN!");
+    // if (js === 'amazing') alert("JavaScript is FUN!");
 
     40 + 8 + 23 - 10;
     console.log(40 + 8 + 23 - 10);
+
+    console.log("Carlos");
+    console.log(23);
+
+    let firstName = "Carlos"; // camel case notation
+
+    console.log(firstName);
+    console.log(firstName);
+
+    let carlos_matilda = "CM";
+
+    let $function = 27;
+
+    let myFirstJob = "Programmer"
+    let myCurrentJob = "Teacher"
+
+    // Not good pratic for clean code.
+    let job1 = "Programmer";
+    let job2 = "Teacher";
+
+    // Practice Assignments
+
+    let country = "Brazil";
+    let continent = "South America";
+    let population = 7000000;
+
+    console.log("Hi my name is: " + firstName + " eu sou do: "+ country + ", localizado no continente: "+ continent + ", com uma população de: " + population+" de habitantes");
