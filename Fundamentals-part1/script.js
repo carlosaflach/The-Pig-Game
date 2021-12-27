644973efa851bbbcfddea7cf6a262759c7e68f8d
Carlos Afonso <carlos.flach15@gmail.com>
2021-12-27 20:51:42 -0300

    Data types.

--- Fundamentals-part1/script.js
+++ Fundamentals-part1/script.js
@@ -1,8 +1,8 @@
-let js = 'amazing';
+/* let js = 'amazing';
     // if (js === 'amazing') alert("JavaScript is FUN!");
 
-    40 + 8 + 23 - 10;
-    console.log(40 + 8 + 23 - 10);
+    // 40 + 8 + 23 - 10;
+    // console.log(40 + 8 + 23 - 10);
 
     console.log("Carlos");
     console.log(23);
@@ -30,3 +30,20 @@ let js = 'amazing';
     let population = 7000000;
 
     console.log("Hi my name is: " + firstName + " eu sou do: "+ country + ", localizado no continente: "+ continent + ", com uma população de: " + population+" de habitantes");
+
+    */
+  let javascriptIsFun = true;
+    // console.log(javascriptIsFun);
+    console.log(typeof javascriptIsFun);
+    // console.log(typeof 'Carlos');
+    // console.log(typeof 23);
+
+    javascriptIsFun = "yes!"
+    console.log(typeof javascriptIsFun);
+
+    let year;
+
+    console.log(year);
+    console.log(typeof year);
+
+    console.log(typeof null); // error from javascript;
