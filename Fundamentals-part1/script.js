110fe04ed3d381559014170648d1f95f7afb88d2
Carlos Afonso <carlos.flach15@gmail.com>
2021-12-27 21:46:05 -0300

    Taking Decisions: if/else statements.

--- Fundamentals-part1/script.js
+++ Fundamentals-part1/script.js
@@ -159,7 +159,7 @@ markHigherBMI = markBMI > johhBMI;
 
 console.log(markHigherBMI);
 
-*/
+
 
 const firstName = "Carlos";
 const job = "Teacher";
@@ -183,3 +183,25 @@ lines");
 console.log(`String
 multiple
 lines`);
+
+*/
+
+const age = 15;
+// const isOldEnough = age >= 18;
+
+if(age >= 18) {
+  console.log('Sarah can start driving license')
+} else {
+  const yearLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearLeft} years =)`);
+}
+
+const birthYear = 1991;
+let century;
+if(birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
