cebb80e5b9c0a9dd6c08230bc6fbad1bd5d28143
Carlos Afonso <carlos.flach15@gmail.com>
2021-12-29 22:39:51 -0300

    The switch statement.

--- Fundamentals-part1/script.js
+++ Fundamentals-part1/script.js
@@ -318,7 +318,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log("Someone else should drive...")
 }
 
-*/
+
 
 // Coding Challenge #3
 
@@ -387,3 +387,44 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log("No one wins the trophy!")
 }
 
+*/
+
+const day = 'monday';
+
+switch(day) {
+  case 'monday': // day === 'monday';
+    console.log('Plan couse structure');
+    console.log('Go to coding meetup');
+    break
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break
+  case 'wednesday':
+  case 'thursday':
+    console.log("Write code examples");
+    break
+  case 'friday':
+    console.log('Record video');
+    break
+  case 'saturday':
+  case 'sunday':
+    console.log("Enjoy the weekend :D");
+    break
+  default :
+    console.log("Not a valid day")
+}
+
+if (day === 'monday') {
+  console.log('Plan couse structure');
+  console.log('Go to coding meetup');
+} else if(day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if(day === 'wednesday' || day === 'thursday'){
+  console.log("Write code examples");
+} else if(day === 'friday') {
+  console.log('Record video');
+} else if(day === 'saturday' || day === 'sunday'){
+  console.log("Enjoy the weekend :D");
+} else {
+  console.log("Not a valid day")
+}
