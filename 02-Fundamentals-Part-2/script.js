f4430a575a61dc7eb6be3e8e4ae51da2927f8c8b
Carlos Afonso <carlos.flach15@gmail.com>
2021-12-30 22:14:16 -0300

    Activating Strict Mode.

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// strict mode avoid us to make some accidentals mistakes.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D")
+
+// const interface = 'Audio';
+// const private = 543;
