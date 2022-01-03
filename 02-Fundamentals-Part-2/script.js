1885a173bc1e064a45b735fc9c3ad6c72d88a694
Carlos Afonso <carlos.flach15@gmail.com>
2022-01-02 22:49:44 -0300

    Fuctions Declarations vs. Expressios.

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -12,7 +12,7 @@ if (hasDriversLicense) console.log("I can drive :D")
 // const interface = 'Audio';
 // const private = 543;
 
-*/
+
 
 function logger() {
   console.log('My name is Carlos');
@@ -36,3 +36,25 @@ console.log(fruitProcessor(2,0));
 
 const appleAndOrangeJuice = fruitProcessor(2, 3);
 console.log(appleAndOrangeJuice);
+
+*/
+
+// fuction declaration
+const age1 = calcAge1(1993);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+console.log(age1);
+
+// fuction expression, anonnymus function.
+// to remember expression produces values;
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1993);
+console.log(age1,age2)
+
+// the difference between fuction declaration and fuction expression, is that when it's function declaration we can invoke the function before it was declarated.
