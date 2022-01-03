5df057bdbb065a54facc638f9041cd31b57229ff
Carlos Afonso <carlos.flach15@gmail.com>
2022-01-02 22:40:36 -0300

    Functions.

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // strict mode avoid us to make some accidentals mistakes.
 
 let hasDriversLicense = false;
@@ -10,3 +11,28 @@ if (hasDriversLicense) console.log("I can drive :D")
 
 // const interface = 'Audio';
 // const private = 543;
+
+*/
+
+function logger() {
+  console.log('My name is Carlos');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor( apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(2, 0);
+
+console.log(appleJuice);
+console.log(fruitProcessor(2,0));
+
+const appleAndOrangeJuice = fruitProcessor(2, 3);
+console.log(appleAndOrangeJuice);
