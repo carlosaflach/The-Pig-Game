ef6dacd35b1ac4fcaf7fb262d720ce861b59ede8
Carlos Afonso <carlos.flach15@gmail.com>
2022-01-18 22:21:47 -0300

    Coding Challenge #2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -180,8 +180,6 @@ console.log(scoreDolphins, scoreKoalas);
 
 checkWinner(scoreDolphins, scoreKoalas);
 
-*/
-
 // Data Structure - Array
 
 // the way that we save data until now
@@ -226,3 +224,78 @@ console.log(age1, age2, age3);
 
 const ages = [age1, age2, age3];
 console.log(ages);
+
+
+// Basic Arrays Methods.
+
+// Add elements.
+const friends = ['Michael', 'Steven', 'Peter'];
+const newLenght = friends.push('Jay'); // Push is a method that it is a function which adds one element in the last position of the array.
+// A push method return the new length of the array that uses this method.
+
+console.log(friends);
+console.log(newLenght);
+
+friends.unshift('John'); // unshift method is a function that adds an element at the first position of an array.
+console.log(friends);
+
+// Remove Elements
+friends.pop(); // last
+const popped = friends.pop(); // the return of a pop function is the element it will be removed.
+console.log(friends);
+console.log(popped);
+
+friends.shift(); // Remove the first element of an array. And also return the element that was removed.
+console.log(friends);
+
+console.log(friends.indexOf('Steven'))
+console.log(friends.indexOf('Bob')) // when we called a indexOf method of an element that is not in the array its return -1
+
+console.log(friends.includes('Steven')); // The includes method will return true if the element is in that array, or false if not.
+console.log(friends.includes('Bob'));
+friends.push(21);
+console.log(friends.includes(21));
+
+if(friends.includes('Peter')) {
+  console.log('You have a friend called Peter');
+} else {
+  console.log('You dont have a friend called Peter')
+}
+
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) 😉
+// GOOD LUCK 😀
+*/
+
+const calcTip = (billValue) => {
+  if(billValue >= 50 && billValue <= 300) {
+    return billValue * 0.15
+  } else {
+    return billValue * 0.20
+  }
+}
+console.log(calcTip(100));
+const bills = [125, 555, 44];
+
+const tips =[calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(bills, tips);
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(totals);
