ac98b6603f28544798a5042f72128aa64c56d857
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-13 09:52:22 -0300

    Handling click events

--- 05-DOM and Events Fundamentals/05-Project-Guess-My-Number/script.js
+++ 05-DOM and Events Fundamentals/05-Project-Guess-My-Number/script.js
@@ -11,3 +11,12 @@ document.querySelector('.score').textContent = 10;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 */
+
+document.querySelector('.check').addEventListener('click',function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+  
+  if (!guess) {
+    document.querySelector('.message').textContent = "⚠️ No Number!⚠️";
+  }
+});
