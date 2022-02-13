5dcdd6f563f2332e86f7c3320d1ae496e17d4b52
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-13 10:55:44 -0300

    Implementing the game logic

--- 05-DOM and Events Fundamentals/05-Project-Guess-My-Number/script.js
+++ 05-DOM and Events Fundamentals/05-Project-Guess-My-Number/script.js
@@ -12,11 +12,37 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click',function() {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(typeof guess);
-  
+
   if (!guess) {
     document.querySelector('.message').textContent = "⚠️ No Number!⚠️";
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎯 Correct Number!'
+  } else if (guess > secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = '⏫ Too high!!'
+      score = score -1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      score = 0;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = '😢 You lost the game!'
+    }
+  } else if (guess < secretNumber) {
+    if(score > 1) {
+      document.querySelector('.message').textContent = '⏬ Too low!!'
+      score = score -1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      score = 0;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = '😢 You lost the game!'
+    }
   }
 });
