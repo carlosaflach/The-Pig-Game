33997bc89615002d1919a1607856e1dff0188e52
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-26 20:13:17 -0300

    Working with classes

--- 05-DOM and Events Fundamentals/05-Project-Modal-Window/script.js
+++ 05-DOM and Events Fundamentals/05-Project-Modal-Window/script.js
@@ -6,5 +6,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for(let i = 0; i < btnsOpenModal.length; i++) 
-  console.log(btnsOpenModal[i].textContent);
+for(let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+  });
+}
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+ 
